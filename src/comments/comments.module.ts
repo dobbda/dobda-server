011aacae4cd2544fabe45afe25d0f1666a0a5fc0
rollrat <rollrat.cse@gmail.com>
2022-07-29 10:37:52 +0900

    fix: Module Import 오류 해결

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnswersRepository } from 'src/answers/repositories/answers.repository';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { CommentsRepository } from './repositories/comments.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentsRepository])],
+  imports: [TypeOrmModule.forFeature([CommentsRepository, AnswersRepository])],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ImagesRepository } from 'src/images/repositories/images.repository';
 import { QuestionController } from './questions.controller';
 import { QuestionsService } from './questions.service';
 import { QuestionsRepository } from './repositories/questions.repository';
 import { QuestionTagsRepository } from './repositories/questionTags.repository';
 import { TagsRepository } from './repositories/tags.repository';
+import { Image } from 'src/images/entities/image.entity';
+import { ImagesModule } from 'src/images/images.module';
 
 @Module({
   imports: [
@@ -12,6 +15,7 @@ import { TagsRepository } from './repositories/tags.repository';
       QuestionsRepository,
       TagsRepository,
       QuestionTagsRepository,
+      ImagesRepository,
     ]),
   ],
   controllers: [QuestionController],
