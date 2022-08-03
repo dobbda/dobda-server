efc9e09443e9cf4bf129aec449b0014a69d5f8f4
rollrat <rollrat.cse@gmail.com>
2022-08-03 22:13:49 +0900

    feat: Coin 엔티티 등록

--- src/coin/coin.module.ts
+++ src/coin/coin.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersRepository } from 'src/users/users.repository';
 import { CoinController } from './coin.controller';
 import { CoinService } from './coin.service';
 import { TransactionsRepository } from './repositories/transactions.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransactionsRepository])],
+  imports: [
+    TypeOrmModule.forFeature([TransactionsRepository, UsersRepository]),
+  ],
   controllers: [CoinController],
   providers: [CoinService],
 })
