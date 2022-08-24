ef0c7b217b725dd49c517c748e382853b6225bae
rollrat <rollrat.cse@gmail.com>
2022-08-24 14:49:09 +0900

    feat(comment): edit/delete 구현

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBody,
   ApiCreatedResponse,
   ApiOperation,
+  ApiParam,
   ApiTags,
+  PartialType,
 } from '@nestjs/swagger';
 import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dtos/create-comment.dto';
+import { EditCommentDto } from './dtos/edit-comment.dto';
 import { GetCommentsDto, GetCommentsOutput } from './dtos/get-comment.dto';
 
 @Controller('comments')
@@ -45,5 +58,39 @@ export class CommentsController {
     @CurrentUser() user: User,
   ) {
     return this.commentsService.createComment(createCommentDto, user);
+  }
+
+  /* 
+    댓글 수정 API
+    url: comments/:id (PATCH)
+  */
+  @Patch('/:id')
+  @ApiOperation({ summary: '댓글 수정' })
+  @ApiParam({ name: 'id', required: true, description: 'comments Id' })
+  @ApiBody({ type: PartialType(CreateCommentDto) })
+  @ApiCreatedResponse({ description: 'id에 해당하는 댓글을 수정한다' })
+  @UseGuards(AccessTokenGuard)
+  async editQuestion(
+    @Param('id') commentId: number,
+    @Body() editCommentDto: EditCommentDto,
+    @CurrentUser() user: User,
+  ) {
+    return this.commentsService.editComment(commentId, editCommentDto, user);
+  }
+
+  /* 
+    댓글 삭제 API
+    url: questions/:id (DELETE)
+  */
+  @Delete('/:id')
+  @ApiOperation({ summary: '댓글 삭제' })
+  @ApiParam({ name: 'id', required: true, description: 'Comment Id' })
+  @ApiCreatedResponse({ description: 'id에 해당하는 댓글을 삭제한다' })
+  @UseGuards(AccessTokenGuard)
+  async deleteQuestion(
+    @Param('id') questionId: number,
+    @CurrentUser() user: User,
+  ) {
+    return this.commentsService.deleteComment(questionId, user);
   }
 }
